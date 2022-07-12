0bf90c8ad9dbfb010d4d498433216df8cd4e165c
Eli Rodriguez <106843292+EliRdrgz@users.noreply.github.com>
2022-07-12 16:11:42 +0200

    generate random number 2

--- src/main/java/com/hooligansofjava/Main.java
+++ src/main/java/com/hooligansofjava/Main.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Game newGame = new Game();
-        newGame.startConsole();
-        FileReadAndWrite.writeFile(newGame);
+
+        /*newGame.startConsole();*/
+        newGame.randomParty();
         newGame.startGame();
+        FileReadAndWrite.writeFile(newGame);
+        /*
         System.out.println(newGame.generateJson());
+        */
     }
 
 
